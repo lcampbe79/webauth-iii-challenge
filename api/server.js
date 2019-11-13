411e3da109349a4c7c5a0cb1cab444bc742c385e
Lisa Campbell <lisa@Lisas-MBP.lan>
2019-11-13 14:58:23 -0800

    Able to Register, Login and GET all users.

--- api/server.js
+++ api/server.js
@@ -4,9 +4,15 @@ const cors = require('cors');
 
 const server = express();
 
+const authRouter = require('../auth/authRouter');
+const usersRouter = require('../users/usersRouter');
+
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+
+server.use('/api/auth', authRouter);
+server.use('/api/users', usersRouter)
 
 server.get('/', (req, res) => {
   res.send('You are now able to become.....')
---
+++ auth/authRouter.js
@@ -0,0 +1,57 @@
+const bcrypt = require('bcryptjs');
+const router = require('express').Router();
+const jwt = require('jsonwebtoken');
+
+const Users = require('../users/usersModel');
+
+router.post('/register', (req, res) => {
+  let user = req.body;
+
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(error).json(error)
+    })
+})
+
+router.post('/login', (req, res) => {
+  let {username, password} = req.body;
+
+
+  Users.findBy({username})
+  .first()
+  .then(user => {
+    if (user && bcrypt.compareSync(password, user.password)) {
+      const token = generateToken(user.username)
+      res.status(200).json({
+        message: `Welcome ${user.username}! Please take this token...`,
+        token
+      });
+    } else {
+      res.status(401).json({ message: 'Invalid Credentials' });
+    }
+  })
+    .catch(error => {
+      console.log(error)
+      res.status(500).json(error)
+    })
+})
+
+function generateToken(username) {
+  const payload = {
+    username,
+    department: 'Front-End'
+  };
+  const secret = process.env.JWT_SECRET || 'secrets are fun'
+  const options = {
+    expiresIn: '1d'
+  }
+  return jwt.sign(payload, secret, options)
+}
+
+module.exports = router;
